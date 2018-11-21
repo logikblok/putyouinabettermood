99b9a6a9ce6acc6b389e6c31f759aaf997dc19ba
Suraj Rai <surajrajrai@gmail.com>
2018-11-21 17:11:04 +0000

    testing

--- libraries/pyiambBot.js
+++ libraries/pyiambBot.js
@@ -26,3 +26,38 @@ pyiamBot.message.bot({
   puppies();
 }
 });
+
+//var kittensarray = ['https://66.media.tumblr.com/e92fa3ce7169de50785e86b7db068055/tumblr_pho8jmStZA1t2ph9to1_640.png', 'https://66.media.tumblr.com/daa744892956d7aae2d3ade6ee1c1b8f/tumblr_phl6kbC0si1t2ph9to1_500.jpg', 'https://66.media.tumblr.com/f28a203d04ca4e01881fc58b2686c0be/tumblr_phkjv0e7921t2ph9to1_500.jpg'];
+//var rand = kittensarray[Math.floor(Math.random() * kittensarray.length)];
+
+var kittens = function () {
+  pyiamBot.message.bot({
+    delay: 1000,
+    content: "Here's a kitten! ![kitten](https://66.media.tumblr.com/3689d86bd754705a3b14d2f17f4e1a4a/tumblr_phpbwdzDTO1t2ph9to1_400.gif)"
+  }).then(function () {
+    return pyiamBot.action.button({
+    delay: 4250,
+    action: [{
+      text:"More Kittens 🐱!",
+      value: "yeskittens"
+    }, {
+      text: "No thanks",
+      value: "end"
+    }]
+  });
+}).then(function (res){
+  if(res.value == 'yeskittens'){
+    kittens();
+  }
+  if(res.value == 'end'){
+  end();
+}
+});
+};
+
+var end = function () {
+  pyiamBot.message.add({
+    delay: 1000,
+    content: 'Thanks for coming. This was created with [Bot UI](https:///docs.botui.org), you can see more cuteness on [Put you in a better mood.com](http://www.putyouinabettermood.com/)'
+  });
+};
